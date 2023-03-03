2c0656fddbd9a360df60f800f21f758effea2f15
Guillem <guillem.hens.98@lacetania.cat>
2023-03-03 20:57:28 +0100

    Comensant factories

--- lacetube-bckend/database/factories/CursFactory.php
+++ lacetube-bckend/database/factories/CursFactory.php
@@ -17,7 +17,13 @@ class CursFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'nick' => fake()->unique()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'rol' => fake()->unique()->numberBetween(0, 1),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            
+
         ];
     }
 }
