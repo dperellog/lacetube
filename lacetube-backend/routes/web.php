eed0e7492dec38577ba320efed657b5b6c20b077
Guillem <guillem.hens.98@lacetania.cat>
2023-03-28 16:13:49 +0200

    Auth users en massa (array de JSON)

--- lacetube-backend/routes/web.php
+++ lacetube-backend/routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Actions\Fortify\CreateNewUser;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+use PhpParser\Node\Stmt\TryCatch;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +26,60 @@ Route::get('/', function () {
 
 Route::middleware('auth:sanctum')->get('/home', function () {
     return 'funciona';
+});
+
+Route::post('/register/json', function (Request $request) {
+    $usersIncorrectes=[];
+    $usersIncorrectes2=[];
+    foreach ($request->usuaris as $user) {
+        try {
+            Validator::make($user, [
+                'nick' => ['required', 'string', 'max:255', Rule::unique(User::class)],
+                'email' => [
+                    'required',
+                    'string',
+                    'email',
+                    'max:255',
+                    Rule::unique(User::class),
+                ],
+            'password' => 'required',
+            ])->validate();
+            User::create($user);
+            array_push($usersIncorrectes2, $user);
+        } catch (\Throwable $th) {
+            
+            $user['error']=$th->getMessage();
+            
+            array_push($usersIncorrectes, $user);
+        }
+        
+    }
+    return response()->json(['status'=>200,'data'=>$usersIncorrectes]);
+    // {
+    //     "usuaris" : [
+    //         {
+    //             "nick": "prova1",
+    //             "email": "prova1@prova.com",
+    //             "password": "1qazZAQ!",
+    //             "password_confirmation": "1qazZAQ!",
+    //             "rol": "admin"
+    //         },
+    //         {
+    //             "nick": "prova2",
+    //             "email": "prova2@prova.com",
+    //             "password": "1qazZAQ!",
+    //             "password_confirmation": "1qazZAQ!",
+    //             "rol": "admin"
+    //         },
+    //         {
+    //             "nick": "prova3",
+    //             "email": "prova3@prova.com",
+    //             "password": "1qazZAQ!",
+    //             "password_confirmation": "1qazZAQ!",
+    //             "rol": "admin"
+    //         }
+    
+    //     ]
+    // }
+    
 });
