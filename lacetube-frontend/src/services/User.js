5872ede7a6caa72ade2bf0ef3c46cafdc24337b8
dperellog <dperellog@gmail.com>
2023-04-17 18:32:42 +0200

    comit

--- lacetube-frontend/src/services/User.js
+++ lacetube-frontend/src/services/User.js
@@ -6,6 +6,11 @@ export default {
         return Service.get('api/user/activities')
     },
 
+    async getCourses() {
+        return Service.get('api/user/courses')
+    },
+
+
     async logout(){
         return Service.get('sanctum/csrf-cookie')
         .then(() => Service.post('logout'))
