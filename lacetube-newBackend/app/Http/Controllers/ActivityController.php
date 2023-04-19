57c2e1343869600ff7f41dd55d026143b511184b
Guillem <guillem.hens.98@lacetania.cat>
2023-04-19 17:55:51 +0200

    commit

--- lacetube-newBackend/app/Http/Controllers/ActivityController.php
+++ lacetube-newBackend/app/Http/Controllers/ActivityController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activity;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+
 
 class ActivityController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function getActivity($id): JsonResponse
     {
-        //
+        return response()->json(Activity::findOrFail($id));
     }
 
     /**
@@ -19,7 +22,8 @@ class ActivityController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $activity = Activity::create($request->all());
+        return response()->json($activity, 201);
     }
 
     /**
@@ -35,7 +39,23 @@ class ActivityController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validateData = $request->validate([
+            'teacher' => 'exists:users,id',
+            'name' => 'required|unique:activity|max:255',
+            'description' => 'required|max:255',
+            'end_date' => 'required|max:255',
+            'course_id' => 'exists:curs,id', // no se si es curs course o courses
+        ]);
+
+        $activity = Activity::find($id);
+
+        $activity->teacher = $request->teacher;
+        $activity->name = $request->name;
+        $activity->description = $request->description;
+        $activity->thumbnailURL = $request->thumbnailURL;
+        $activity->year = $request->year;
+        $activity->parent = $request->parent;
+        $activity->save();
     }
 
     /**
