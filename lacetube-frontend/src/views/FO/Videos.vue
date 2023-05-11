4efcc903bec3542372467c889871894f160e6c30
dperellog <dperellog@gmail.com>
2023-05-11 15:41:52 +0200

    commit

---
+++ lacetube-frontend/src/views/FO/Videos.vue
@@ -0,0 +1,31 @@
+<template>
+  <HeaderFrontoffice></HeaderFrontoffice>
+  <div class="container mt-4 px-4">
+    <h1 class="fw-bold">Els meus videos:</h1>
+    <hr>
+      <ElsMeusCursos class="my-5" mostrarTots></ElsMeusCursos>
+    
+  </div>
+  <FooterFrontoffice></FooterFrontoffice>
+</template>
+  
+<script>
+import HeaderFrontoffice from '@/components/FO/HeaderFrontoffice.vue';
+import FooterFrontoffice from '@/components/FO/FooterFrontoffice.vue';
+import ElsMeusCursos from '@/components/FO/tauler/sections/ElsMeusCursos.vue';
+
+export default {
+  components: {
+    HeaderFrontoffice,
+    FooterFrontoffice,
+    ElsMeusCursos
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
